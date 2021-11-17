486f6364148fcf6b3f4a0e95b9097cda65493608
Lena <tsybulenko.helen@gmail.com>
2021-11-17 19:18:47 +0300

    upd

--- skin/components/blocks/News.js
+++ skin/components/blocks/News.js
@@ -5,10 +5,14 @@ const News = () => {
     return(
         <>
         {/*<div className={block_styles.title}>Новости</div>*/}
-        <ul>
+        <ul className={styles.list}>
             <li className={styles.link}>
                 <img src={'https://avatars.mds.yandex.net/get-ynews-logo/50744/1551-1563808847385-square/logo-square'} />
                 Кабмин утвердил перечень районов Крайнего Севера и приравненных к ним территорий
+            </li>
+            <li className={styles.link}>
+                <img src={'https://avatars.mds.yandex.net/get-ynews-logo/135513/1040-1478692902361-square/logo-square'} />
+                В квартире задержанного по подозрению в педофилии депутата от КПРФ Самсонова идут обыски
             </li>
         </ul>
         </>
